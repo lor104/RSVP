855fdee1b6a99014603dcb2bfe18d970b706366e
Lauren Rosentzveig <lauren@richmondday.com>
2019-11-21 10:54:16 -0500

    init

---
+++ RSVP/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RSVP.Infrastucture.Data;
+using RSVP.Infrastucture.Models.ViewModels;
+
+namespace RSVP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (RSVPEntities db = new RSVPEntities())
+            {
+                //Event firstEvent = db.Events.FirstOrDefault(x => x.Details != null && x.Description != null);
+
+                List<Event> events = db.Events.ToList();
+                List<Guest> guests = db.Guests.ToList();
+
+                EventViewModel viewModel = new EventViewModel()
+                {
+                    Events = events,
+                    Guests = guests
+                };
+
+                return View(viewModel);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RSVP/Infrastucture/Models/ViewModels/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RSVP.Infrastucture.Data;
+
+namespace RSVP.Infrastucture.Models.ViewModels
+{
+    public class EventViewModel
+    {
+        public List<Event> Events { get; set; }
+        public List<Guest> Guests { get; set; }
+    }
+}
