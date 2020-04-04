143291b828b802564655d2f559eab0a9724c67e8
Lauren Rosentzveig <lauren@richmondday.com>
2020-04-04 13:40:49 -0400

    design pages

--- RSVP/Controllers/HomeController.cs
+++ RSVP/Controllers/HomeController.cs
@@ -137,19 +137,5 @@ namespace RSVP.Controllers
 
             return View(model);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
